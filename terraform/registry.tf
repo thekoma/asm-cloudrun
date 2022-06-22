a3d5ac2ee3f92739e54a858c20d66f539df5097a
Andrea Cervesato <andrea.cervesato@gmail.com>
2022-06-22 13:09:10 +0000

    added permissions

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -7,3 +7,14 @@ resource "google_artifact_registry_repository" "demo-repo" {
   description   = "Demo Registry"
   format        = "DOCKER"
 }
+
+
+resource "google_artifact_registry_repository_iam_member" "registry-access" {
+  project       = module.project-factory.project_id
+  depends_on    = [google_artifact_registry_repository.demo-repo]
+  provider = google-beta
+  location = google_artifact_registry_repository.demo-repo.location
+  repository = google_artifact_registry_repository.demo-repo.name
+  role   = "roles/artifactregistry.reader"
+  member = "serviceAccount:${module.gke.service_account}"
+}
