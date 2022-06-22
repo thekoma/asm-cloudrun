d287a8323f8da7c814689fb32011dbe42765412b
Andrea Cervesato <andrea.cervesato@gmail.com>
2022-06-22 08:03:35 +0000

    Non funziona un beneamato

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
 provider "google" {
   credentials = "./credentials.json"
+  region      = var.region
+}
+
+provider "google-beta" {
+  credentials = "./credentials.json"
+  region      = var.region
 }
 
 module "project-factory" {
@@ -11,7 +17,24 @@ module "project-factory" {
   org_id               = var.org_id
   billing_account      = var.billing_account
   activate_apis        = [
-    "anthos.googleapis.com"
+    "compute.googleapis.com"
   ]
   auto_create_network = true
+}
+
+
+resource "google_compute_firewall" "monitoring" {
+  name    = "monitoring"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
 }
